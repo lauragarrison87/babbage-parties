38ca94e94ed3a12bf8682cf1313dad34644e2d3a
David Grellscheid <david.grellscheid@uib.no>
2024-04-06 11:54:52 +0200

    Allow person to have blank birth/death dates. Run ./manage.py migrate

--- babbage/parties/models.py
+++ babbage/parties/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Person(models.Model):
     qid = models.PositiveBigIntegerField(primary_key=True)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=False)
     presumed = models.BooleanField(default=False)
-    birth = models.DateField()
-    death = models.DateField()
+    birth = models.DateField(null=True)
+    death = models.DateField(null=True)
 
 
 
