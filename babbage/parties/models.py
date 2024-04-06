056b278ad6943a878dce065b09ad8deeba482a7d
David Grellscheid <david.grellscheid@uib.no>
2024-04-06 11:31:43 +0200

    Initial database model for "Person", not filled. Apply with "./manage.py migrate"

--- babbage/parties/models.py
+++ babbage/parties/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    qid = models.PositiveBigIntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    presumed = models.BooleanField(default=False)
+    birth = models.DateField()
+    death = models.DateField()
+
+
+
