f2ec0034565fb71813b2673d996799c8e9626c4b
David Grellscheid <david.grellscheid@uib.no>
2024-04-06 11:56:02 +0200

    Covert qid data to YAML, populate database with "./manage.py loaddata guest_qid"

---
+++ guest_qid_to_yaml.py
@@ -0,0 +1,28 @@
+import csv
+
+template = """\
+- model: parties.person
+  pk: {qid}
+  fields:
+    name: "{name}"
+    presumed: {presume}
+
+"""
+
+with open("guest_qid.csv", newline="", encoding="utf8") as f:
+    next(f)
+    csvfile = csv.reader(f)
+    for line in csvfile:
+        try:
+            name, qid, presume = line
+        except ValueError:
+            name, presume = line
+            qid = presume
+        if qid == "Unknown":
+            qid = 'Q24238356' # "entity whose identity is not known"
+        presume = bool(presume)
+        qid = int(qid.strip()[1:])
+        print(template.format(qid=qid, name=name, presume=presume))
+        
+
+
