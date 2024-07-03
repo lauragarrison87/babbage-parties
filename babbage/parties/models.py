9d3d032d8985cb74b39af54e5d861079d23d49d5
David Grellscheid <david.grellscheid@uib.no>
2024-07-03 12:42:15 +0200

    Completely rework the csv reader. Delete database, then manage.py migrate, then manage.py loaddata initial

--- babbage/parties/models.py
+++ babbage/parties/models.py
@@ -21,9 +21,19 @@ class Person(models.Model):
         ordering = ["name", "qid"]
 
 
+class Book(models.Model):
+    bid = models.CharField("Book ID", max_length=80, primary_key=True)
+    citation = models.CharField(max_length=80, null=True)
+
+    def __str__(self):
+        return self.bid
+    
+    class Meta:
+        ordering = ["bid"]
+
 class Source(models.Model):
     sid = models.CharField("Source ID", max_length=80, primary_key=True)
-    source = models.CharField(max_length=80, null=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     quote = models.TextField(null=False)
     pages = models.CharField(max_length=80, null=True)
 
@@ -32,6 +42,7 @@ class Source(models.Model):
     
     class Meta:
         ordering = ["sid"]
+
 
 
 class Party(models.Model):
